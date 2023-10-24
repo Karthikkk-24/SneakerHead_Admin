6ca3a5b24776a020e016ffa2e3491718337a59d1
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-24 14:16:31 +0530

    Added index

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import App from '@app/App';
-// import store from '@store/store';
+import App from '@app/App';
+import store from '@store/store';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-// import './utils/i18n';
-// import './index.css';
-// import * as serviceWorker from './serviceWorker';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import './utils/i18n';
 
-// declare const window: any;
+declare const window: any;
 
-// window.PF = {
-//   config: {
-//     mode: 'bs4',
-//   },
-// };
+window.PF = {
+  config: {
+    mode: 'bs4',
+  },
+};
 
-// const container: any = document.getElementById('root');
-// const root = createRoot(container);
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+const container: any = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
