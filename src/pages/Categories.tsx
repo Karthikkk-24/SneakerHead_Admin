e3629856db4fc0bf0127d4b3fc07f1a847f24d79
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-24 17:51:07 +0530

    Added Category Page

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 const Blank = () => {
   return (
     <div>
-      <ContentHeader title="Banner" />
+      <ContentHeader title="Category" />
       <section className="content">
         <div className="container-fluid">
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Categories</h3>
+              <h3 className="card-title">Create Category</h3>
               <div className="card-tools">
                 <button
                   type="button"
@@ -21,21 +21,37 @@ const Blank = () => {
                 >
                   <i className="fa fa-minus" />
                 </button>
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="remove"
-                  data-toggle="tooltip"
-                  title="Remove"
-                >
-                  <i className="fa fa-times" />
-                </button>
               </div>
             </div>
             <div className="card-body">
-              Start creating your amazing application!
+              <div className="col-md-6 form-group">
+                <label htmlFor="category_name" className="form-label">Category Name</label>
+                <input type="text" name="category_name" id="categoryName" placeholder='Enter Category Name' className="form-control" />
+              </div>
             </div>
-            <div className="card-footer">Footer</div>
+            <div className="card-footer">
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+                <div className="card-title">Category List</div>
+            </div>
+            <div className="card-body">
+                <div className="table-responsive">
+                    <table className="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>Sr. No</th>
+                                <th>Category Name</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
           </div>
         </div>
       </section>
