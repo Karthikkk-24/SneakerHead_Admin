5185802198e2ab32d45d10813abe12d6b769a1cf
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-25 08:59:10 +0530

    Added Accessories Page

--- src/pages/Accessories.tsx
+++ src/pages/Accessories.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { ContentHeader } from '@components';
-import React from 'react';
+import { ContentHeader } from "@components";
+import React from "react";
 
 const Blank = () => {
   return (
     <div>
-      <ContentHeader title="Banner" />
+      <ContentHeader title="Accessories" />
       <section className="content">
         <div className="container-fluid">
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Banner</h3>
+              <h3 className="card-title">Create Accessories</h3>
               <div className="card-tools">
                 <button
                   type="button"
@@ -21,21 +21,74 @@ const Blank = () => {
                 >
                   <i className="fa fa-minus" />
                 </button>
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="remove"
-                  data-toggle="tooltip"
-                  title="Remove"
-                >
-                  <i className="fa fa-times" />
-                </button>
               </div>
             </div>
             <div className="card-body">
-              Start creating your amazing application!
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <label htmlFor="categoryName" className="form-label">
+                    Select Product
+                  </label>
+                  <select
+                    name="category_name"
+                    id="categoryName"
+                    className="form-control"
+                  >
+                    <option value="">Select Product</option>
+                  </select>
+                </div>
+                <div className="col-md-6 form-group">
+                  <label htmlFor="AccessoryName" className="form-label">
+                    Enter Accessories Name
+                  </label>
+                  <input
+                    type="text"
+                    name="accessory_name"
+                    id="AccessoryName"
+                    placeholder="Enter Accessories Name"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col-md-6 form-group">
+                  <label htmlFor="AccessoryPrice" className="form-label">
+                    Enter Accessories Price
+                  </label>
+                  <input
+                    type="text"
+                    name="accessory_price"
+                    id="AccessoryPrice"
+                    placeholder="Enter Accessories Price"
+                    className="form-control"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="card-footer">Footer</div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <div className="card-title">Accessories List</div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Product Name</th>
+                      <th>Accessories Name</th>
+                      <th>Accessories Price</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody></tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </section>
