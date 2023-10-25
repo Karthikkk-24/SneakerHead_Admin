38d3c96cb905420307629299dcce81b1b502455a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-25 09:00:45 +0530

    Changes to footer

--- src/modules/main/footer/Footer.tsx
+++ src/modules/main/footer/Footer.tsx
@@ -1,6 +1,6 @@
+import { DateTime } from 'luxon';
 import React from 'react';
-import {useTranslation} from 'react-i18next';
-import {DateTime} from 'luxon';
+import { useTranslation } from 'react-i18next';
 import packageJSON from '../../../../package.json';
 
 const Footer = () => {
@@ -10,8 +10,8 @@ const Footer = () => {
     <footer className="main-footer">
       <strong>
         <span>Copyright © {DateTime.now().toFormat('y')} </span>
-        <a href="https://erdkse.com" target="_blank" rel="noopener noreferrer">
-          erdkse.com
+        <a href="" target="_blank" rel="noopener noreferrer">
+          Karthik Shettigar (the Coding Hacker)
         </a>
         <span>.</span>
       </strong>
