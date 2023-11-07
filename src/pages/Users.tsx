11685f85d74321503934c74372601bc37fbef6b4
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-07 23:02:50 +0530

    Changed Users page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,16 +1,37 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { ContentHeader } from '@components';
-import React from 'react';
+import { ContentHeader } from "@components";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const Blank = () => {
+const Users = () => {
+
+    const [accessories, setAccessories] = useState([]);
+
+    useEffect(() => {
+        fetchAccessories();
+    });
+
+    function fetchAccessories() {
+        axios
+          .get("http://localhost:3000/api/getAccessories")
+          .then((response) => {
+            console.log(response.data[0]);
+            setAccessories(response.data[0]);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+
+
   return (
     <div>
-      <ContentHeader title="Banner" />
+      <ContentHeader title="Average Order" />
       <section className="content">
         <div className="container-fluid">
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Banner</h3>
+              <h3 className="card-title">Create Accessories</h3>
               <div className="card-tools">
                 <button
                   type="button"
@@ -21,21 +42,84 @@ const Blank = () => {
                 >
                   <i className="fa fa-minus" />
                 </button>
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="remove"
-                  data-toggle="tooltip"
-                  title="Remove"
-                >
-                  <i className="fa fa-times" />
-                </button>
               </div>
             </div>
             <div className="card-body">
-              Start creating your amazing application!
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <label htmlFor="categoryName" className="form-label">
+                    Select Product
+                  </label>
+                  <select
+                    name="category_name"
+                    id="categoryName"
+                    className="form-control"
+                  >
+                    <option value="">Select Product</option>
+                  </select>
+                </div>
+                <div className="col-md-6 form-group">
+                  <label htmlFor="AccessoryName" className="form-label">
+                    Enter Accessories Name
+                  </label>
+                  <input
+                    type="text"
+                    name="accessory_name"
+                    id="AccessoryName"
+                    placeholder="Enter Accessories Name"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col-md-6 form-group">
+                  <label htmlFor="AccessoryPrice" className="form-label">
+                    Enter Accessories Price
+                  </label>
+                  <input
+                    type="text"
+                    name="accessory_price"
+                    id="AccessoryPrice"
+                    placeholder="Enter Accessories Price"
+                    className="form-control"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="card-footer">Footer</div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <div className="card-title">Average Order List</div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Product Name</th>
+                      <th>Accessories Name</th>
+                      <th>Accessories Price</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {accessories.map((accessory, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{accessory.product_name}</td>
+                        <td>{accessory.accessories_name}</td>
+                        <td>{accessory.accessories_price}</td>
+                        <td><button className="btn btn-outline-primary"><i className="fa-regular fa-pen-to-square"></i></button>&emsp;<button className="btn btn-outline-danger"><i className="fa-regular fa-trash-can"></i></button></td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -43,4 +127,4 @@ const Blank = () => {
   );
 };
 
-export default Blank;
+export default Users;
