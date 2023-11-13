faee6f346b00610335f2b1265f2ba7756f5f3eb4
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-13 08:39:11 +0530

    User Notification

--- src/pages/UserNotification.tsx
+++ src/pages/UserNotification.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { ContentHeader } from '@components';
-import React from 'react';
+import { ContentHeader } from "@components";
+import React from "react";
 
-const Blank = () => {
+const UserNotification = () => {
   return (
     <div>
-      <ContentHeader title="Banner" />
+      <ContentHeader title="User Notification" />
       <section className="content">
         <div className="container-fluid">
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Banner</h3>
+              <h3 className="card-title">Add User Notification</h3>
               <div className="card-tools">
                 <button
                   type="button"
@@ -21,21 +21,43 @@ const Blank = () => {
                 >
                   <i className="fa fa-minus" />
                 </button>
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="remove"
-                  data-toggle="tooltip"
-                  title="Remove"
-                >
-                  <i className="fa fa-times" />
-                </button>
               </div>
             </div>
             <div className="card-body">
-              Start creating your amazing application!
+                <div className="col-md-12 row">
+                    <div className="col-md-6 col-sm-12 form-group">
+                        <label htmlFor="">Notification</label>
+                        <input type="text" className="form-control" placeholder="Notification" />
+                    </div>
+                    <div className="col-md-6 col-sm-12 form-group">
+                        <label htmlFor="">Description</label>
+                        <input type="text" className="form-control" placeholder="Description" />
+                    </div>
+                </div>
             </div>
-            <div className="card-footer">Footer</div>
+            <div className="card-footer">
+                <button className="btn btn-primary" type="Submit">Submit</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">Notification List</h3>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Notification</th>
+                      <th>Description</th>
+                      <th>Date</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -43,4 +65,4 @@ const Blank = () => {
   );
 };
 
-export default Blank;
+export default UserNotification;
