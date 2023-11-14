0291870cff1d0df66aab82d48f55781709e40f1b
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-14 08:50:25 +0530

    Ratings Done

--- src/pages/Ratings.tsx
+++ src/pages/Ratings.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 const Ratings = () => {
   return (
     <div>
-      <ContentHeader title="Banner" />
+      <ContentHeader title="Ratings" />
       <section className="content">
         <div className="container-fluid">
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Banner</h3>
+              <h3 className="card-title">1<sup> st</sup></h3>
               <div className="card-tools">
                 <button
                   type="button"
@@ -21,21 +21,142 @@ const Ratings = () => {
                 >
                   <i className="fa fa-minus" />
                 </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Name</th>
+                      <th>Ratings</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">2<sup> nd</sup></h3>
+              <div className="card-tools">
                 <button
                   type="button"
                   className="btn btn-tool"
-                  data-widget="remove"
+                  data-widget="collapse"
                   data-toggle="tooltip"
-                  title="Remove"
+                  title="Collapse"
                 >
-                  <i className="fa fa-times" />
+                  <i className="fa fa-minus" />
                 </button>
               </div>
             </div>
             <div className="card-body">
-              Start creating your amazing application!
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Name</th>
+                      <th>Ratings</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
             </div>
-            <div className="card-footer">Footer</div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">3<sup> rd</sup></h3>
+              <div className="card-tools">
+                <button
+                  type="button"
+                  className="btn btn-tool"
+                  data-widget="collapse"
+                  data-toggle="tooltip"
+                  title="Collapse"
+                >
+                  <i className="fa fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Name</th>
+                      <th>Ratings</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">4<sup> th</sup></h3>
+              <div className="card-tools">
+                <button
+                  type="button"
+                  className="btn btn-tool"
+                  data-widget="collapse"
+                  data-toggle="tooltip"
+                  title="Collapse"
+                >
+                  <i className="fa fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Name</th>
+                      <th>Ratings</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">5<sup> th</sup></h3>
+              <div className="card-tools">
+                <button
+                  type="button"
+                  className="btn btn-tool"
+                  data-widget="collapse"
+                  data-toggle="tooltip"
+                  title="Collapse"
+                >
+                  <i className="fa fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>Sr. No</th>
+                      <th>Name</th>
+                      <th>Ratings</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </section>
