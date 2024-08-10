7d9d9d70d75141cc645715855021b5645ef57b80
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-11 00:45:07 +0530

    Code Formatting & Added Font

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,37 +1,37 @@
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function NavItem({
-  title,
-  path,
-  active,
+    title,
+    path,
+    active,
 }: {
-  title: string;
-  path: string;
-  active: boolean;
+    title: string;
+    path: string;
+    active: boolean;
 }) {
-  return (
-    <Link
-      to={path}
-      className={`w-full h-10 pl-3 ${
-        active
-          ? "bg-primary font-semibold rounded-lg text-background"
-          : "text-white"
-      } flex items-center justify-start text-xl`}
-    >
-      {title}
-    </Link>
-  );
+    return (
+        <Link
+            to={path}
+            className={`w-full h-10 pl-3 ${
+                active
+                    ? 'bg-primary font-semibold rounded-lg text-background'
+                    : 'text-white'
+            } flex items-center justify-start text-xl`}
+        >
+            {title}
+        </Link>
+    );
 }
 
 NavItem.propTypes = {
-  title: PropTypes.string,
-  path: PropTypes.string,
-  active: PropTypes.bool,
+    title: PropTypes.string,
+    path: PropTypes.string,
+    active: PropTypes.bool,
 };
 
 NavItem.defaultProps = {
-  title: "",
-  path: "",
-  active: false,
+    title: '',
+    path: '',
+    active: false,
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,36 +1,40 @@
-import PropTypes from "prop-types";
-import React from "react";
-import NavItem from "./NavItem";
+import PropTypes from 'prop-types';
+import React from 'react';
+import NavItem from './NavItem';
 
 export default function Sidebar({ currentPath }: { currentPath: string }) {
-  return (
-    <div className="w-full h-full flex flex-col items-start justify-center p-5 gap-4 bg-background rounded-2xl">
-      <NavItem title="Home" path={"/"} active={currentPath === "/"} />
-      <NavItem
-        title="Projects"
-        path={"/projects"}
-        active={currentPath === "/projects"}
-      />
-      <NavItem
-        title="Blogs"
-        path={"/blogs"}
-        active={currentPath === "/blogs"}
-      />
-      <NavItem title="Shop" path={"/shop"} active={currentPath === "/shop"} />
-      <NavItem
-        title="About"
-        path={"/about"}
-        active={currentPath === "/about"}
-      />
-      <NavItem
-        title="Contact"
-        path={"/contact"}
-        active={currentPath === "/contact"}
-      />
-    </div>
-  );
+    return (
+        <div className="w-full h-full flex flex-col items-start justify-center p-5 gap-4 bg-background rounded-2xl">
+            <NavItem title="Home" path={'/'} active={currentPath === '/'} />
+            <NavItem
+                title="Projects"
+                path={'/projects'}
+                active={currentPath === '/projects'}
+            />
+            <NavItem
+                title="Blogs"
+                path={'/blogs'}
+                active={currentPath === '/blogs'}
+            />
+            <NavItem
+                title="Shop"
+                path={'/shop'}
+                active={currentPath === '/shop'}
+            />
+            <NavItem
+                title="About"
+                path={'/about'}
+                active={currentPath === '/about'}
+            />
+            <NavItem
+                title="Contact"
+                path={'/contact'}
+                active={currentPath === '/contact'}
+            />
+        </div>
+    );
 }
 
 Sidebar.propTypes = {
-  currentPath: PropTypes.string,
+    currentPath: PropTypes.string,
 };
