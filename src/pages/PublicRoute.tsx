b72b62dfd213b530c15a244d32a0491c493726db
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-11 00:35:23 +0530

    Added PublicRoute, Login & Register

---
+++ src/pages/PublicRoute.tsx
@@ -0,0 +1,21 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import Sidebar from './Sidebar';
+
+const PublicRoute = () => {
+    const location = useLocation();
+
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
+            <div className="w-full h-full flex items-start justify-start">
+                <div className="w-96 h-full flex flex-col items-start justify-start p-6">
+                    <Sidebar currentPath={location.pathname} />
+                </div>
+                <div className="w-full h-full flex flex-col items-start justify-start p-6">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PublicRoute;
