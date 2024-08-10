747d7c92ca2dc72cee8dff48da8c0be92dfe7b1a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-11 00:37:51 +0530

    Changes to sidebar and added code for PublicRoute

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+export default function Sidebar({ currentPath }: { currentPath: string }) {
+  return <div></div>;
+}
+
+Sidebar.propTypes = {
+  currentPath: PropTypes.string,
+};
